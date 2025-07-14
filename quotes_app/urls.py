37993b1d3c9687b642d03789a2f27bfad03b3c4c
valerialev14 <lerrika10@mail.ru>
2025-07-14 10:12:51 +0300

    Update urls.py

--- quotes_app/urls.py
+++ quotes_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
@@ -8,4 +9,8 @@ urlpatterns = [
     path('top-quotes/', views.TopQuotesView.as_view(), name='top_quotes'),
     path('worst-quotes/', views.WorstQuotesView.as_view(), name='worst_quotes'),
     path('delete-quote/', views.DeleteQuoteView.as_view(), name='delete_quote'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('delete/<int:quote_id>/', views.delete_quote, name='delete_quote'),
 ]
